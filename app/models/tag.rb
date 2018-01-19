b9fc44bc5bec21b392bd184915edbf1316cc8ca9
Rmoreno <FearNoMore>
2018-01-19 21:12:49 +0300

    fixed request to db

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -4,6 +4,7 @@ class Tag < ActiveRecord::Base
   has_many :posts, through: :taggings
 
   def self.counts
-    self.select("name, count(taggings.tag_id) as count").joins(:taggings).group("taggings.tag_id")
+    # self.select("name, count(taggings.tag_id) as count").joins(:taggings).group("taggings.tag_id")
+    self.select("name, count(taggings.tag_id) as count").joins(:taggings).group("name, taggings.tag_id")
   end
 end
